e734ec7f5a1d19466255a727279199c9fb506e49
Tom B <53711814+tombalcombe@users.noreply.github.com>
2019-08-10 13:45:33 +0100

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -4,7 +4,7 @@ Created on Fri Aug  9 18:11:11 2019
 
 @author: thoma
 """
-
+import time
 codeindex = 0
 stringlist = []
 
@@ -15,6 +15,7 @@ cipher3_input = input("Type in cipher3 here:" )
 cipher = (int(cipher1_input),int(cipher2_input),int(cipher3_input))
 
 def encode():
+    global stringlist
     global codeindex
     xindex = 0
     yindex = 0
@@ -28,6 +29,7 @@ def encode():
         
 def decode():
     global codeindex
+    global stringlist
     xindex = 0
     yindex = 0
     for i in stringlist[xindex:]:
@@ -39,6 +41,7 @@ def decode():
     codeindex = codeindex - 1
     
 def printX():
+    global stringlist
     chrlist = []
     for i in stringlist:
         chrlist.append(chr(i))
@@ -49,28 +52,41 @@ def printX():
     for i in printy:
         f.write(i)
     chrlist.clear()
+    f.close()
     start()
+
 def quitoK():
     quit()
+    time.sleep(1)
+    return()
+    quitoK()
     
 def start():
-    global string_input
     global stringlist
     print("stringlist:",stringlist)
     print("cipher:",cipher, "index:", codeindex)
-    print("Type + (for add index), - (for deduct index), p (to print string) or q (to exit)...")
-    print("Or type i to import a string from export.txt")
+    print("Type:")
+    print("+ for add index (cipher)") 
+    print("- for deduct index (cipher)")
+    print("p to print out stored string to export.txt")
+    print("i to import a string from export.txt")
+    print("s to input your own string.")
+    print("q to quit")
     x = input("Type here:" )
-    if x == 's':
+    if x == "s":
+        stringlist.clear()
         string_input = input("Type in string here:" )
-        stringlist = []
         for i in string_input:
             stringlist.append(ord(i))
+        start()
     if x == "i":
+        stringlist.clear()
         f1 = open("export.txt","r")
         string_input = f1.read()
         for i in string_input:
             stringlist.append(ord(i))
+        f1.close()
+        start()
     if x == "+":
         encode()
     if x == "-":
